c52c36c4cd88f75335d86395a6dcd9fa496e0521
Venessa Huang <vhuang@lcs.on.ca>
2026-02-24 14:37:29 -0500

    Finished one row of the World Clock time zone interface

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -10,8 +10,47 @@ import SwiftUI
 struct WorldClockView: View {
     var body: some View {
         NavigationStack {
-            Text("World Clock")
+            VStack{
+                //Ottawa
+                HStack{
+                    //left side
+                    VStack{
+                        Text("Today, +0HRS")
+                        Text("Ottawa")
+                            .font(.system(.largeTitle, design: .default, weight: .thin))
+                    }
+                    
+                    Spacer()
+                    
+                    //Right side
+                    Text("6:35")
+                        .font(.system(size: 64.0, weight: .thin, design: .default))
+                    Text("AM")
+                        .font(.system(.largeTitle, design: .default, weight: .thin))
+                    
+                    
+                }
+            }
                 .navigationTitle("World Clock")
+                .toolbar {
+                    
+                    ToolbarItem(placement: .topBarLeading) {
+                        
+                        Button("Edit") {
+                            // Does nothing right now
+                        }
+                        
+                    }
+                    
+                    ToolbarItem(placement: .primaryAction) {
+                        
+                        Button {
+                            // Does nothing right now
+                        } label:{Image(systemName: "plus")}
+                        
+                    }
+                    
+                }
         }
     }
 }
