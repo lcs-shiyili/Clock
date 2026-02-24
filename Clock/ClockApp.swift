db35edf17d34208eed1fec01617a80f9743feaf5
Venessa Huang <vhuang@lcs.on.ca>
2026-02-24 14:08:33 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by 黃翊喬 on 2026/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
